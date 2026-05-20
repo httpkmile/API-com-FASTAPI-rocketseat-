493fdd22ced6c8f7c411466c5f6a1eec3df9d214
Camile Rodrigues <awscamile@outlook.com>
2026-05-20 16:23:18 -0300

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import uvicorn 
 
 if __name__ =="__main__":
-    uvicorn .run ("src.main.server.server:app",host ="127.0.0.1",port =8000 ,reload =True )
+    uvicorn .run ("src.main.server.server:app",host ="localhost",port =0000 ,reload =True )
     #inicia o servidor FastAPI usando Uvicorn, especificando o módulo e a aplicação a ser executada, além de configurar o host, porta e habilitar o modo de recarga automática para desenvolvimento.
